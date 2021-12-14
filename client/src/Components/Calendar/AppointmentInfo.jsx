3b1b2b10ef631020cdc6ed7e4b5df95c4dc2ec6a
Charlie Paik <chpaik88@gmail.com>
2021-12-13 23:47:40 -0800

    customized custom popup window

--- client/src/Components/Calendar/AppointmentInfo.jsx
+++ client/src/Components/Calendar/AppointmentInfo.jsx
@@ -6,6 +6,8 @@ const AppointmentInfo = ({data}) => {
   return (
     <div id='appointment-info'>
       <h2>{subject}</h2>
+      <img src={data.ImgURL} />
+      <div>{data.description}</div>
     </div>
   )
 }
