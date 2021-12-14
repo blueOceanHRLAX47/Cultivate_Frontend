9d9c0f4c51ad11155db61f12278d4356ce9a2666
NicholasAnich <infinitevolume@gmail.com>
2021-12-14 12:03:59 -0800

    changed app view to recipes instead of forum

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -20,11 +20,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const AllRecipesAndWorkouts = React.createContext();
 
 const App = () => {
-<<<<<<< HEAD
   const [view, setView] = useState('recipes')
-=======
-  const [view, setView] = useState('calendar')
->>>>>>> 3fb2be3b9ecc7257e771211adf979531971c1987
   const [recipes, setRecipes] = useState()
   // const [savedRecipes, setSavedRecipes] = useState([])
 
