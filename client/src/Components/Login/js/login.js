175a76e2624018ac134c8991375bf5238f0e6b1d
Yi Qiao <yiqial4@uci.edu>
2021-12-14 10:02:49 -0800

    Updated

--- client/src/Components/Login/js/login.js
+++ client/src/Components/Login/js/login.js
@@ -1,62 +1,68 @@
-$("input[type='password'][data-eye]").each(function(i) {
+$("input[type='password'][data-eye]").each(function (i) {
   var $this = $(this),
-    id = 'eye-password-' + i,
-    el = $('#' + id);
+    id = "eye-password-" + i,
+    el = $("#" + id);
 
-  $this.wrap($("<div/>", {
-    style: 'position:relative',
-    id: id
-  }));
+  $this.wrap(
+    $("<div/>", {
+      style: "position:relative",
+      id: id,
+    })
+  );
 
   $this.css({
-    paddingRight: 60
+    paddingRight: 60,
   });
-  $this.after($("<div/>", {
-    html: 'Show',
-    class: 'btn btn-primary btn-sm',
-    id: 'passeye-toggle-'+i,
-  }).css({
-      position: 'absolute',
+  $this.after(
+    $("<div/>", {
+      html: "Show",
+      class: "btn btn-primary btn-sm",
+      id: "passeye-toggle-" + i,
+    }).css({
+      position: "absolute",
       right: 10,
-      top: ($this.outerHeight() / 2) - 12,
-      padding: '2px 7px',
+      top: $this.outerHeight() / 2 - 12,
+      padding: "2px 7px",
       fontSize: 12,
-      cursor: 'pointer',
-  }));
+      cursor: "pointer",
+    })
+  );
 
-  $this.after($("<input/>", {
-    type: 'hidden',
-    id: 'passeye-' + i
-  }));
+  $this.after(
+    $("<input/>", {
+      type: "hidden",
+      id: "passeye-" + i,
+    })
+  );
 
-  var invalid_feedback = $this.parent().parent().find('.invalid-feedback');
+  var invalid_feedback = $this.parent().parent().find(".invalid-feedback");
 
-  if(invalid_feedback.length) {
+  if (invalid_feedback.length) {
     $this.after(invalid_feedback.clone());
   }
 
-  $this.on("keyup paste", function() {
-    $("#passeye-"+i).val($(this).val());
+  $this.on("keyup paste", function () {
+    $("#passeye-" + i).val($(this).val());
   });
-  $("#passeye-toggle-"+i).on("click", function() {
-    if($this.hasClass("show")) {
-      $this.attr('type', 'password');
+  $("#passeye-toggle-" + i).on("click", function () {
+    if ($this.hasClass("show")) {
+      $this.attr("type", "password");
       $this.removeClass("show");
       $(this).removeClass("btn-outline-primary");
-    }else{
-      $this.attr('type', 'text');
-      $this.val($("#passeye-"+i).val());
+    } else {
+      $this.attr("type", "text");
+      $this.val($("#passeye-" + i).val());
       $this.addClass("show");
       $(this).addClass("btn-outline-primary");
     }
   });
 });
 
-$(".my-login-validation").submit(function() {
+$(".my-login-validation").submit(function () {
   var form = $(this);
-      if (form[0].checkValidity() === false) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  form.addClass('was-validated');
+  if (form[0].checkValidity() === false) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  form.addClass("was-validated");
 });
