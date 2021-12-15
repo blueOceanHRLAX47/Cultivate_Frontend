513ad8768fc447a1cf057ab9d84f388b5eb515b9
joshayres <me@joshayres.tech>
2021-12-14 16:35:54 -0800

    server work plz

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use('/home', express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
