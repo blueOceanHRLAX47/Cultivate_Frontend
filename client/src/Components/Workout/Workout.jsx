3f886cdba999e40ab83a897031d7395e32db0a3f
Mike Ortiz <emike108@aim.com>
2021-12-14 17:06:52 -0800

    added asynch capabilities through rgenerator-runtime, edited webpack

--- client/src/Components/Workout/Workout.jsx
+++ client/src/Components/Workout/Workout.jsx
@@ -1,18 +1,30 @@
 import React, { useState, useContext } from 'react';
 import { Container } from 'react-bootstrap';
-import { AllRecipesAndWorkouts } from '../App';
+// import { AllRecipesAndWorkouts } from '../App';
 import WorkoutItem from './WorkoutItem'
+import axios from 'axios';
+import { results } from '../../Contexts/forumContext';
+import 'regenerator-runtime/runtime'
 
 
 const Workout = () => {
-  const { workoutData } = useContext(AllRecipesAndWorkouts)
-  console.log('at workout', workoutData.workout)
+  // const { workoutData } = useContext(AllRecipesAndWorkouts)
+  const [workoutData, changeWorkoutData] = useState([])
+
+  axios.get('http://localhost:3002/')
+    .then(results => {
+      console.log('test', results.data)
+      changeWorkoutData(results.data)
+    })
+    .catch(err => console.error(err))
+
+    console.log('at workout', workoutData)
 
   return (
     <Container className="primary-container">
       <h1 className="mb-20">Workout</h1>
       <Container className="exercise-list">
-        {workoutData.workout.map((workout, index) =>
+        {workoutData.map((workout, index) =>
           <WorkoutItem workout={workout} key={index} />
         )}
       </Container>
