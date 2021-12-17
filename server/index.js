86da0ee703f635c1187ae66aacc08a4a0682842e
joshayres <me@joshayres.tech>
2021-12-17 11:15:33 -0800

    bundle smallified

--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 app.use(cors());
+
+app.get('*.js', (req, res, next) => {
+  req.url = req.url + '.gz';
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
+  next();
+})
+
 app.use('/home', express.static(path.join(__dirname, '../client/dist')));
 //don't forget to add '/home' back to the previous line before commiting
 app.use(express.json());
